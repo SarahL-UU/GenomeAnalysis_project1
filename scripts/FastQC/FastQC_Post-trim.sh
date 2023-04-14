a84d9aacd141c44ee51bb861610a978229458630
SarahL-UU <Sarah.Lideus.4744@student.uu.se>
2023-04-14 13:50:15 +0200

    FastQC of the data

---
+++ scripts/FastQC/FastQC_Post-trim.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -A uppmax2023-2-8
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 1
+#SBATCH -t 00:15:00
+#SBATCH -J FastQC_Illumina
+#SBATCH -o FastQC_Illumina.log
+#SBATCH -e FastQC_Illumina.err
+#SBATCH --mail-user Sarah.Lideus.4744@student.uu.se
+#SBATCH --mail-type=ALL
+
+
+#Load modules
+module load bioinfo-tools
+module load FastQC/0.11.9
+
+#Quality check genomics
+fastqc -o /home/sarahl/GenomeAnalysis_project1/001-Analyses/Genomic_analyses/Quality_check_DNA_data/Illumina/Post-trimming /home/sarahl/GenomeAnalysis_project1/001-Analyses/Genomic_analyses/Trimmed_Illumina_WGS/*
